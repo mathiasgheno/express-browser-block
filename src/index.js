1f98d7594e20f29b1ec4df23d2eb978e3e0b8928
mathias <mathiasgheno@gmail.com>
2022-04-15 19:37:18 -0300

    docs: add jsdocs for blockRequestIfBrowserIsNot

--- src/index.js
+++ src/index.js
@@ -20,6 +20,39 @@ const conditionsDefault = {
   opera: ">=22",
 }
 
+/**
+ * @typedef BrowserConditions
+ * @property {object} windows
+ * @property {object} macos
+ * @property {object} mobile
+ * @property {string} chrome
+ * @property {string} firefox
+ * @property {string} opera
+ */
+
+/**
+ * @description
+ *
+ * # What this does
+ * This function is going to return the middleware funtion that will block the request's user if the browser does not satisfies.
+ * The first argument is the coditions. See [Browser documentation for more dails](https://github.com/lancedikson/bowser#filtering-browsers).
+ * The secund argument is the HTML that will be returned if the browser does not satisfies.
+ *
+ * # How to use
+ * The simplest way to use it is throught express `.use`.
+ *
+ * ```js
+ * import express from 'express';
+ * import { blockRequestIfBrowserIsNot } from 'express-browser-block';
+ * const app = express();
+ *
+ * app.use(blockRequestIfBrowserIsNot());
+ * ```
+ *
+ * @param conditions {BrowserConditions}
+ * @param html {string}
+ * @returns {(function(req, res, next): void)|*}
+ */
 export const blockRequestIfBrowserIsNot = (conditions = conditionsDefault, html = htmlDefault) => {
   return (req, res, next) => {
     const browser = Bowser.getParser(req.headers['user-agent']);
